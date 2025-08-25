18cf636e9ef288da6ea696fb4b08ffb909f8e170
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-08-25 16:14:58 +0700

    add api promotion

---
+++ src/main/java/com/sarinah/sales/controller/ScanPromotionController.java
@@ -0,0 +1,23 @@
+package com.sarinah.sales.controller;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.sarinah.sales.service.PostScanBarcodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sarinah-forwarder/v1/modul")
+public class ScanPromotionController {
+    private final PostScanBarcodeService postScanBarcodeService;
+
+    @PostMapping(value = "/barcode")
+    public ArrayNode postLoyaltyMemberResponse(@RequestBody ObjectNode request) {
+        return postScanBarcodeService.execute(request);
+    }
+}
