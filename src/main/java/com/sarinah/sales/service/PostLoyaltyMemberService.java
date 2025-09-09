54deaa719ddcb7d61461d1cf8247af5686ef969d
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-09-09 14:30:04 +0700

    [IMPROVE] improve response loyalty member

--- src/main/java/com/sarinah/sales/service/PostLoyaltyMemberService.java
+++ src/main/java/com/sarinah/sales/service/PostLoyaltyMemberService.java
@@ -1,10 +1,13 @@
 package com.sarinah.sales.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sarinah.sales.adaptor.SarinahGetModulServiceAdaptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +15,37 @@ public class PostLoyaltyMemberService {
     private final SarinahGetModulServiceAdaptor sarinahGetModulServiceAdaptor;
 
 
-
     public ArrayNode execute(ObjectNode request) {
-       return sarinahGetModulServiceAdaptor.getLoyalty(request);
+       ArrayNode response = sarinahGetModulServiceAdaptor.getLoyalty(request);
+        for (JsonNode node : response) {
+            if (node.isObject()) {
+                ObjectNode obj = (ObjectNode) node;
 
+                // hapus field yang tidak perlu
+                obj.remove(Arrays.asList(
+                        "is_loyalty",
+                        "is_customer",
+                        "is_property",
+                        "mobile",
+                        "barcode",
+                        "loyalty_id"
+                ));
+
+                // masking phone
+                if (obj.hasNonNull("phone")) {
+                    String phone = obj.get("phone").asText();
+                    int len = phone.length();
+                    if (len > 3) {
+                        obj.put("phone", phone.substring(0, len - 3) + "***");
+                    } else {
+                        obj.put("phone", "***");
+                    }
+                }
+            }
+        }
+        return response;
     }
+
+
+
 }
